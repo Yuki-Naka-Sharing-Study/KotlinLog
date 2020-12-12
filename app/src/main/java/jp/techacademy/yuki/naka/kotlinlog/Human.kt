58d732c96fb4674c10bbd28603664c9a63de7c59
Yuki-Naka-Sharing-Study <yukinaka2168@au.com>
2020-12-12 22:10:09 +0900

    「Humanクラス」の「: super」の後に続く「hobby」は削除しました。

--- app/src/main/java/jp/techacademy/yuki/naka/kotlinlog/Human.kt
+++ app/src/main/java/jp/techacademy/yuki/naka/kotlinlog/Human.kt
@@ -7,7 +7,7 @@ open class Human: Animal, Thinkable {
     var hobby: String
 
     // 引数付きコンストラクタ
-    constructor(name: String, age: Int, hobby: String): super(name, age, hobby) {
+    constructor(name: String, age: Int, hobby: String): super(name, age) {
         this.hobby = ""
     }
 
